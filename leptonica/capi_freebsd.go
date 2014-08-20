d3cf0f8a630cbbf5625ebfb199684712211e5b56
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-08-20 10:48:14 +0900

    freebsd: specify LDFLAGS by hand for workaround.

--- leptonica/capi_freebsd.go
+++ leptonica/capi_freebsd.go
@@ -3,5 +3,11 @@
 package leptonica
 
 // #cgo CFLAGS: -I/usr/local/include
-// #cgo pkg-config: lept
+// #cgo LDFLAGS: -L/usr/local/lib -llept
 import "C"
+
+/*
+Note: specify LDFLAGS by hand for workaround.
+BUG: FreeBSD 10 release lept.pc is not set ${prefix} and ${libdir}.
+This bug causes compilation error.
+*/
