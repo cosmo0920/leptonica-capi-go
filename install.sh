ee564ce58f18f4e34a082f2300afa6a02e280ee8
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-08-21 23:09:36 +0900

    add travis settings

---
+++ install.sh
@@ -0,0 +1,12 @@
+sudo apt-get install zlib1g-dev
+sudo apt-get install libpng12-dev libjpeg62-dev libtiff4-dev libsdl-gfx1.2-dev libsdl-image1.2-dev libsdl1.2-dev libavcodec-dev libavdevice-dev libavformat-dev libavutil-dev
+sudo apt-get install python-enchant python-poppler
+
+cd /tmp
+wget http://www.leptonica.com/source/leptonica-1.70.tar.gz
+tar -xvzof leptonica-1.70.tar.gz
+cd leptonica-1.70
+./configure
+make
+sudo make install
+sudo ldconfig
