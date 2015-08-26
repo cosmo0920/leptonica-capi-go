272167af3364335dfcb675859f52b5a5cb6f4904
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2015-08-26 20:40:58 +0900

    Use ENV to make easy maintain

--- install.sh
+++ install.sh
@@ -2,10 +2,11 @@ sudo apt-get install zlib1g-dev
 sudo apt-get install libpng12-dev libjpeg62-dev libtiff4-dev libsdl-gfx1.2-dev libsdl-image1.2-dev libsdl1.2-dev libavcodec-dev libavdevice-dev libavformat-dev libavutil-dev
 sudo apt-get install python-enchant python-poppler
 
+export LEPTONICA_VERSION="1.71"
 cd /tmp
-wget http://www.leptonica.com/source/leptonica-1.71.tar.gz
-tar -xvzof leptonica-1.71.tar.gz
-cd leptonica-1.71
+wget http://www.leptonica.com/source/leptonica-${LEPTONICA_VERSION}.tar.gz
+tar -xvzof leptonica-${LEPTONICA_VERSION}.tar.gz
+cd leptonica-${LEPTONICA_VERSION}
 ./configure
 n_processors="$(grep '^processor' /proc/cpuinfo | wc -l)"
 make -j${n_processors}
