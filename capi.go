3f13b70d2dc48dfb609d19ff40ea5a17fa02f810
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2015-09-06 15:58:44 +0900

    Extract constants to another file

--- capi.go
+++ capi.go
@@ -10,52 +10,6 @@ import (
 	"unsafe"
 )
 
-type CopyFlag int32
-const (
-	L_INSERT CopyFlag = iota
-	L_COPY
-	L_CLONE
-	L_COPY_CLONE
-)
-
-type IMGFormat int32
-const (
-	IFF_UNKNOWN IMGFormat = iota
-	IFF_BMP
-	IFF_JFIF_JPEG
-	IFF_PNG
-	IFF_TIFF
-	IFF_TIFF_PACKBITS
-	IFF_TIFF_RLE
-	IFF_TIFF_G3
-	IFF_TIFF_G4
-	IFF_TIFF_LZW
-	IFF_TIFF_ZIP
-	IFF_PNM
-	IFF_PS
-	IFF_GIF
-	IFF_JP2
-	IFF_WEBP
-	IFF_LPDF
-	IFF_DEFAULT
-	IFF_SPIX
-)
-
-type GrayChoose int32
-const (
-	L_CHOOSE_MIN = 1 + iota
-	L_CHOOSE_MAX
-	L_CHOOSE_MAX_MIN_DIFF
-)
-
-type ColorMap int32
-const (
-	REMOVE_CMAP_TO_BINARY = iota
-	REMOVE_CMAP_TO_GRAYSCALE
-	REMOVE_CMAP_TO_FULL_COLOR
-	REMOVE_CMAP_BASED_ON_SRC
-)
-
 func Version() string {
 	cVersion := C.getLeptonicaVersion()
 	version := C.GoString(cVersion)
