83a93cbdbe48e47606761f351c257a7ba3fb9389
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2015-09-16 08:46:57 +0900

    Add ConvertRGBToGrayMinMax usage sample

---
+++ example/sample_grayscale2.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	lept "github.com/cosmo0920/leptonica-capi-go"
+)
+
+func main() {
+	version := lept.Version()
+	fmt.Println("leptonica version: " + version)
+	targetFile := "伊号潜水艦.png"
+	pix, err := lept.PixRead(targetFile)
+
+	if err != nil {
+		panic("Could not read specified png file.")
+	}
+
+	fmt.Println("ConvertRGBToGrayFast:", targetFile)
+	tpix, err := pix.ConvertRGBToGrayMinMax(lept.L_CHOOSE_MAX)
+
+	if err != nil {
+		panic("Could not convert specified pix to grayscale.")
+	}
+
+	tpix.PixWrite("Translated2.png", lept.IFF_PNG)
+}
