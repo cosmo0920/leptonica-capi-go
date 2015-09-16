ebe7af239b4d3c836b03760e3641572bd06a5059
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2015-09-17 08:41:45 +0900

    Add .ConvertRGBToGray example

---
+++ example/sample_grayscale3.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	lept "github.com/cosmo0920/leptonica-capi-go"
+)
+
+func main() {
+	version := lept.Version()
+	fmt.Println("leptonica version: " + version)
+	targetFile := "伊号潜水艦.png"
+	pix, err := lept.PixRead(targetFile)
+
+	if err != nil {
+		panic("Could not read specified png file.")
+	}
+
+	fmt.Println("ConvertRGBToGray:", targetFile)
+	tpix, err := pix.ConvertRGBToGray(0.35, 0.35, 0.3)
+
+	if err != nil {
+		panic("Could not convert specified pix to grayscale.")
+	}
+
+	tpix.PixWrite("Translated3.png", lept.IFF_PNG)
+}
