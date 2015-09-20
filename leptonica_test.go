92419d9b57c702e80672feec9e412714dc5c60c0
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2015-09-20 16:20:29 +0900

    Add test cases
    * ConvertRGBToGrayFast
    * ConvertRGBToGrayMinMax
    * ConvertRGBToGray

--- leptonica_test.go
+++ leptonica_test.go
@@ -1,13 +1,50 @@
 package leptonica_test
 
 import (
-	lept "."
+	lept "github.com/cosmo0920/leptonica-capi-go"
+	"path/filepath"
 	"testing"
 )
+
+func setUp() (*lept.Pix) {
+	targetFile := filepath.Join("example", "伊号潜水艦.png")
+	pix, _ := lept.PixRead(targetFile)
+	return pix;
+}
 
 func TestVersion(t *testing.T) {
 	result := lept.Version()
 	if result == "" {
 		t.Errorf("result = %v cannot empty string", result)
+	}
+}
+
+func TestConvertRGBToGrayFast(t *testing.T) {
+	pix := setUp()
+
+	_, err := pix.ConvertRGBToGrayFast()
+
+	if err != nil {
+		t.Errorf("Could not convert specified pix to grayscale.")
+	}
+}
+
+func TestConvertRGBToGrayMinMax(t *testing.T) {
+	pix := setUp()
+
+	_, err := pix.ConvertRGBToGrayMinMax(lept.L_CHOOSE_MAX)
+
+	if err != nil {
+		t.Errorf("Could not convert specified pix to grayscale.")
+	}
+}
+
+func TestConvertRGBToGray(t *testing.T) {
+	pix := setUp()
+
+	_, err := pix.ConvertRGBToGray(0.35, 0.35, 0.3)
+
+	if err != nil {
+		t.Errorf("Could not convert specified pix to grayscale.")
 	}
 }
