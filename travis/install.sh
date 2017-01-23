2298e12c5bdfadc31837bb6051d0104891e75285
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2017-01-23 14:14:25 +0900

    Use leptonica 1.74

--- travis/install.sh
+++ travis/install.sh
@@ -3,7 +3,7 @@ sudo apt-get install zlib1g-dev
 sudo apt-get install libpng12-dev libjpeg62-dev libtiff4-dev libsdl-gfx1.2-dev libsdl-image1.2-dev libsdl1.2-dev libavcodec-dev libavdevice-dev libavformat-dev libavutil-dev
 sudo apt-get install python-enchant python-poppler
 
-export LEPTONICA_VERSION="1.71"
+export LEPTONICA_VERSION="1.74"
 cd /tmp
 wget http://www.leptonica.com/source/leptonica-${LEPTONICA_VERSION}.tar.gz
 tar -xvzof leptonica-${LEPTONICA_VERSION}.tar.gz
